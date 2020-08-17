a491c6ddc694658448511a2082d331f9ae6b8abf
Michael Chen <michaelchengaming@gmail.com>
2020-08-17 00:00:36 -0700

    configure get and post for the testing app

--- server.js
+++ server.js
@@ -7,21 +7,47 @@ const todos = [
 ];
 
 const server = http.createServer((req, res) => {
-  // ALWAYS HAVE CONTENT TYPE IN HEADER
-  // res.statusCode = 404;
-  // res.setHeader("Content-Type", "application/json");
-  // res.setHeader("X-Powered-By", "Node.js");
-  res.writeHead(400, {
-    "Content-Type": "application/json",
-    "X-Powered-By": "Node.js",
-  });
-  res.end(
-    JSON.stringify({
-      success: false,
-      error: "Wheres yo email brah",
-      data: null,
+  const { method, url } = req;
+
+  let body = [];
+  req
+    .on("data", (chunk) => {
+      body.push(chunk);
     })
-  );
+    .on("end", () => {
+      body = Buffer.concat(body).toString();
+
+      let status = 404;
+
+      const response = {
+        success: false,
+        data: null,
+        error: null,
+      };
+
+      if (method === "GET" && url === "/todos") {
+        status = 200;
+        response.success = true;
+        response.data = todos;
+      } else if (method === "POST" && url === "/todos") {
+        const { id, text } = JSON.parse(body);
+        if (!id || !text) {
+          status = 400;
+          response.error = "id and text brah";
+        } else {
+          todos.push({ id, text });
+          status = 201;
+          response.success = true;
+          response.data = todos;
+        }
+      }
+
+      res.writeHead(status, {
+        "Content-Type": "application/json",
+        "X-Powered-By": "Node.js",
+      });
+      res.end(JSON.stringify(response));
+    });
 });
 
 const PORT = 5000;
